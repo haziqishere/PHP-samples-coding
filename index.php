3f56ef51e1f155d765a1be8dbe252942320c1ec5
Haziq Hakimi <haziqhakimi02@gmail.com>
2024-05-20 11:04:01 +0800

    add variables into SQL line

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
     // Connect to database
     include("database.php");
+
+    $username = "Squidward";
+    $password = "clarinet2";
+
+    $sql = "INSERT INTO user (username, password)
+            VALUES ('$username', '$password')";
+
+    try{
+        // Insert new user into database
+        mysqli_query($conn, $sql);
+        echo"User is now registered";
+    }
+    catch(mysqli_sql_exception) {
+        echo"Could not register user";
+    }
+
+    
+    // Close connection
+    mysqli_close($conn);
 ?>
 <!DOCTYPE html>
 <html lang="en">
